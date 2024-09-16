8071ff8d3c6cedadc6ae08db10b50c8962b6441e
JJuani <juanijimenez809@gmail.com>
2024-09-16 15:26:09 -0300

    detail product form

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 9)]
+PRODUCT_SIZE_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
 
 class CartAddProductForm(forms.Form):
@@ -13,6 +14,20 @@ class CartAddProductForm(forms.Form):
                                           'style': 'width: 80px;',
                                       })
                                       )
+    override = forms.BooleanField(required=False,
+                                  initial=False,
+                                  widget=forms.HiddenInput)
+
+
+class CartAddProductSizeForm(forms.Form):
+    size = forms.TypedChoiceField(choices=PRODUCT_SIZE_CHOICES,
+                                  coerce=int,
+                                  label=_('Talle'),
+                                  widget=forms.Select(attrs={
+                                      'class': 'cantidad.-control',
+                                      'style': 'width: 80px;',
+                                  }))
+
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
