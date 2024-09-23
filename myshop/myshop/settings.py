ce6d26460428c31c7eaa1b15a2fe984f1b954998
JJuani <juanijimenez809@gmail.com>
2024-09-23 18:19:17 -0300

    final mail in settings

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -153,7 +153,7 @@ CART_SESSION_ID = 'cart'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'seriedrago@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
