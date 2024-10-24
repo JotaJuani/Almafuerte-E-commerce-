76801df444f75b02f10f5484fbe5dcc976683f8e
jotajuani <juanijimenez809@gmail.com>
2024-10-24 09:54:13 -0300

    ports corrected to 8080

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -e 
-python manage.py collectstatic -noinput 
-
-uwsgi --socket :8000 --master --enable-threads --module app.wsgi 
+python manage.py collectstatic --noinput 
+python manage.py migrate
+uwsgi --socket :8080 --master --enable-threads --buffer-size 32768 --module myshop.wsgi 
