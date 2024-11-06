6a16207bc3d8ff889b740508655a384f5e071f80
jotajuani <juanijimenez809@gmail.com>
2024-11-06 20:26:47 -0300

    login and register corrected

--- myshop/account/forms.py
+++ myshop/account/forms.py
@@ -4,16 +4,19 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
-        class Meta:
-            model = User
-            fields = ['username', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
 
-
-        def __init__(self, *args, **kwargs):
-            super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-            self.fields['username'].widget.attrs.update(
-                {'class': 'form-control', 'placeholder': 'Ingresa nombre de usuario...'})
-            self.fields['password1'].widget.attrs.update(
-                {'class': 'form-control', 'placeholder': 'Ingresa tu contraseña...'})
-            self.fields['password2'].widget.attrs.update(
-                {'class': 'form-control', 'placeholder': 'Repeti tu contraseña...'})
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].label = 'Nombre de usuario'
+        self.fields['username'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Ingresa nombre de usuario...'})
+        self.fields['password1'].label = 'Contraseña'
+        self.fields['password1'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Ingresa tu contraseña...'})
+        self.fields['password2'].label = 'Repetir contraseña'
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Repeti tu contraseña...'})
