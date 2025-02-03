f2c23a220d31dbcd0f54d255808aa19df94a6fad
jotajuani <juanijimenez809@gmail.com>
2025-02-02 22:38:27 -0300

    collecstatic fixed

--- static/css/js/script.js
+++
@@ -1,9 +0,0 @@
-const navbarEl = document.querySelector('.navbar');
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 450){
-        navbarEl.classList.add('navbar-scrolled');
-    } else {
-        navbarEl.classList.remove('navbar-scrolled');
-    }
-});
-
--- static/css/js/search_glass.js
+++
@@ -1,9 +0,0 @@
-function expandSearch() {
-    var searchInput = document.querySelector('.search-click');
-    searchInput.classList.toggle('expanded');
-    if (searchInput.classList.contains('expanded')) {
-        searchInput.focus();
-    } else {
-        searchInput.blur();
-    }
-}
