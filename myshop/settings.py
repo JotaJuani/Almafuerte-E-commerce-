07f79149947e94cd9bd6119be900fdf62d50ee19
jotajuani <juanijimenez809@gmail.com>
2025-02-11 22:45:04 -0300

    without static root

--- myshop/settings.py
+++ myshop/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 AWS_S3_CUSTOM_DOMAIN = "ortopediashopbucket.s3.amazonaws.com"
