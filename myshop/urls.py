bf7e72a1e02cc08059011868299faaed73d01a6e
jotajuani <juanijimenez809@gmail.com>
2025-02-12 23:12:05 -0300

    cart url moved up

--- myshop/urls.py
+++ myshop/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('theboss/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
     path('contact/', include('contact.urls', namespace='contact')),
-    path('cart/', include('cart.urls', namespace='cart')),
+    
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('account/', include('account.urls', namespace='account'))
